bd98b58588d7d645a385ddf0a9691be33fc21c04
hydRAnger <armyiljfe@gmail.com>
2023-06-18 23:47:24 +0900

    chore: inline-style for bgColor

--- components/UseCaseCard.tsx
+++ components/UseCaseCard.tsx
@@ -1,18 +1,33 @@
-import { UseCardProps } from "@/types"
+import { UseCardProps } from "@/types";
 
 const UseCaseCard = (props: UseCardProps) => {
-    const bgColor = props.backgroudColor ? props.backgroudColor : "#FF6F2D0F"
-    return (
-        <div className={`flex flex-col justify-stretch space-y-[8px] box-border rounded-[15px] border-2 border-solid border-[#828282] bg-[${bgColor}] w-[314px] h-[235px] pb-[15px] p-[18px]`}>
-            <p className="text-[18px] font-medium leading-2 text-[#4F4F4F] line-clamp-2 flex-shrink-0">{props.title}</p>
-            <p id="summary" className="text-[13px] leading-4 text-[#4F4F4F] text-ellipsis overflow-hidden">{props.summary}</p>
-            <div className='grow'></div>
-            <p className='text-[12px] font-medium flex-shrink-0'>The demo needs Polygon token to work with.</p>
-            <div className="mx-[3px] font-medium text-black text-[16px] h-[45px] flex-shrink-0 box-border rounded-[15px] border-2 border-solid border-[#828282] bg-white">
-                <p className="text-center m-0">Go</p>
-            </div>
-        </div>
-    )
-}
+  const bgColor = props.backgroudColor ? props.backgroudColor : "#FF6F2D0F";
 
-export default UseCaseCard
+  console.log(`======== bgColor: ${bgColor} ========}`);
+
+  return (
+    <div
+      className={`flex flex-col justify-stretch space-y-[8px] box-border rounded-[15px] border-2 border-solid border-[#828282] bg-[${bgColor}] w-[314px] h-[235px] pb-[15px] p-[18px]`}
+      style={{ backgroundColor: bgColor }}
+    >
+      <p className="text-[18px] font-medium leading-2 text-[#4F4F4F] line-clamp-2 flex-shrink-0">
+        {props.title}
+      </p>
+      <p
+        id="summary"
+        className="text-[13px] leading-4 text-[#4F4F4F] text-ellipsis overflow-hidden"
+      >
+        {props.summary}
+      </p>
+      <div className="grow"></div>
+      <p className="text-[12px] font-medium flex-shrink-0">
+        The demo needs Polygon token to work with.
+      </p>
+      <div className="mx-[3px] font-medium text-black text-[16px] h-[45px] flex-shrink-0 box-border rounded-[15px] border-2 border-solid border-[#828282] bg-white">
+        <p className="text-center m-0">Go</p>
+      </div>
+    </div>
+  );
+};
+
+export default UseCaseCard;
