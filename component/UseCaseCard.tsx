b68da87a292c0ff24d06301341ea65b82d36025c
吴崧 <oct.song.wu@gmail.com>
2023-06-19 09:33:32 +0800

    Revert "fix: css sytle import"
    This reverts commit 2040f8298d93df14263ec4261e9bd6874a1942c6.

--- component/UseCaseCard.tsx
+++ component/UseCaseCard.tsx
@@ -3,14 +3,14 @@ import { UseCardProps } from "@/types"
 const UseCaseCard = (props: UseCardProps) => {
     const bgColor = props.backgroudColor ? props.backgroudColor : "#FF6F2D0F"
     return (
-        <div className={`flex flex-col justify-stretch space-y-[8px] box-border rounded-[15px] border-2 border-solid border-[#828282] bg-[${bgColor}] w-[314px] h-[235px] pb-[15px] p-[18px]`}>
+        <div className={`flex flex-col space-y-[8px] justify-between box-border rounded-[15px] border-2 border-solid border-[#828282] bg-[${bgColor}] w-[314px] h-[235px] pb-[15px] p-[18px]`}>
             <p className="text-[18px] font-medium leading-2 text-[#4F4F4F] line-clamp-2 flex-shrink-0">{props.title}</p>
             <p id="summary" className="text-[13px] leading-4 text-[#4F4F4F] text-ellipsis overflow-hidden">{props.summary}</p>
             <div className='grow'></div>
             <p className='text-[12px] font-medium flex-shrink-0'>The demo needs Polygon token to work with.</p>
-            <div className="mx-[3px] font-medium text-black text-[16px] h-[45px] flex-shrink-0 box-border rounded-[15px] border-2 border-solid border-[#828282] bg-white">
-                <p className="text-center m-0">Go</p>
-            </div>
+            <div className="mx-[3px] font-medium text-black text-[16px] h-[45px] flex-shrink-0 box-border rounded-[15px] border-2 border-solid border-[#828282] bg-white"
+            // onClick={ () => {window.open(props.link)}}
+            >Go</div>
         </div>
     )
 }
