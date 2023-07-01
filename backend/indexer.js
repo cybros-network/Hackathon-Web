08aaa67f11784f441b8118e4f0f07a151ccd39c1
Cybros CI <dev@cybros.network>
2023-07-01 19:44:34 +0000

    backend: deno fmt

--- backend/indexer.js
+++ backend/indexer.js
@@ -2,8 +2,8 @@ import { ApiPromise, Keyring, WsProvider } from "https://deno.land/x/polkadot/ap
 import { cryptoWaitReady } from "https://deno.land/x/polkadot/util-crypto/mod.ts";
 import { sleep } from "https://deno.land/x/sleep/mod.ts";
 
-const POOL_ID = 102
-const POOL_IMPL_ID = 1
+const POOL_ID = 102;
+const POOL_IMPL_ID = 1;
 
 export const KEY_QUEUE_SIZE = ["QUEUE_SIZE"];
 export const KEY_PROCESSED_HEIGHT = ["PROCESSED_HEIGHT"];
@@ -57,39 +57,39 @@ async function indexer(kv, api) {
 
     const curr = processedHeight + 1;
     const batch = kv.atomic();
-    const jobCache = {}
+    const jobCache = {};
     const getJob = async (id) => {
       if (jobCache[id]) {
-        return jobCache[id]
+        return jobCache[id];
       }
-      const q = await kv.get([...KEY_PREFIX_JOB, id])
+      const q = await kv.get([...KEY_PREFIX_JOB, id]);
       if (!q.value) {
-        return null
+        return null;
       }
-      jobCache[id] = q.value
-      return jobCache[id]
-    }
+      jobCache[id] = q.value;
+      return jobCache[id];
+    };
     const tryGetJob = async (id) => {
-      const ret = await getJob(id)
+      const ret = await getJob(id);
       if (!ret) {
-        throw new Error('Cached job not found!')
+        throw new Error("Cached job not found!");
       }
-      return ret
-    }
+      return ret;
+    };
 
-    const hash = await api.rpc.chain.getBlockHash(curr)
-    const apiAt = await api.at(hash)
-    const events = (await apiAt.query.system.events()).toHuman()
-    const ts = (await apiAt.query.timestamp.now()).toJSON()
+    const hash = await api.rpc.chain.getBlockHash(curr);
+    const apiAt = await api.at(hash);
+    const events = (await apiAt.query.system.events()).toHuman();
+    const ts = (await apiAt.query.timestamp.now()).toJSON();
 
     for (const e of events) {
-      if (e.event.section !== 'offchainComputing') continue
-      let job
-      const data = e.event.data
+      if (e.event.section !== "offchainComputing") continue;
+      let job;
+      const data = e.event.data;
       switch (e.event.method) {
-        case 'JobCreated':
+        case "JobCreated":
           jobCache[data.jobId] = {
-            status: 'pending',
+            status: "pending",
             createdIn: curr,
             jobId: data.jobId,
             poolId: data.poolId,
@@ -100,41 +100,41 @@ async function indexer(kv, api) {
             assignment: null,
             result: null,
             createdAt: ts,
-            updatedAt: Date.now()
-          }
-          console.log(`Job #${data.jobId} created in block #${curr}`)
+            updatedAt: Date.now(),
+          };
+          console.log(`Job #${data.jobId} created in block #${curr}`);
           break;
-        case 'JobAssigned':
-          job = await tryGetJob(data.jobId)
+        case "JobAssigned":
+          job = await tryGetJob(data.jobId);
           jobCache[data.jobId] = {
             ...job,
-            status: 'generating',
+            status: "generating",
             updatedAt: Date.now(),
             assignment: {
               assignee: data.assignee,
               createdIn: curr,
-              createdAt: ts
-            }
-          }
-          console.log(`Job #${data.jobId} assigned to ${data.assignee} in block #${curr}`)
+              createdAt: ts,
+            },
+          };
+          console.log(`Job #${data.jobId} assigned to ${data.assignee} in block #${curr}`);
           break;
-        case 'JobStatusUpdated':
+        case "JobStatusUpdated":
           // currently ignored
           break;
-        case 'JobResultUpdated':
-          job = await tryGetJob(data.jobId)
+        case "JobResultUpdated":
+          job = await tryGetJob(data.jobId);
           jobCache[data.jobId] = {
             ...job,
-            status: 'done',
+            status: "done",
             updatedAt: Date.now(),
             result: {
               status: data.result,
               output: data.output,
               createdIn: curr,
-              createdAt: ts
-            }
-          }
-          console.log(`Job #${data.jobId} finished in block #${curr}`)
+              createdAt: ts,
+            },
+          };
+          console.log(`Job #${data.jobId} finished in block #${curr}`);
           break;
         default:
           break;
@@ -142,18 +142,21 @@ async function indexer(kv, api) {
     }
 
     for (const v of Object.values(jobCache)) {
-      batch.set([...KEY_PREFIX_JOB, v.jobId], v)
+      batch.set([...KEY_PREFIX_JOB, v.jobId], v);
     }
 
-    const nextId = (await apiAt.query.offchainComputing.nextJobId(POOL_ID)).toJSON()
-    batch.set(KEY_LATEST_JOB, typeof nextId === 'number' ? nextId - 1 : 0)
+    const nextId = (await apiAt.query.offchainComputing.nextJobId(POOL_ID)).toJSON();
+    batch.set(KEY_LATEST_JOB, typeof nextId === "number" ? nextId - 1 : 0);
     batch.set(KEY_PROCESSED_HEIGHT, curr);
     await batch.commit();
     processedHeight = curr;
 
     if (targetHeight <= processedHeight) {
       targetHeight = (await api.derive.chain.bestNumberFinalized()).toNumber();
-      await kv.set(KEY_QUEUE_SIZE, (await api.query.offchainComputing.assignableJobs.entries(POOL_ID, POOL_IMPL_ID)).length)
+      await kv.set(
+        KEY_QUEUE_SIZE,
+        (await api.query.offchainComputing.assignableJobs.entries(POOL_ID, POOL_IMPL_ID)).length,
+      );
     }
   }
 }
